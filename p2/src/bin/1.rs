89b3b4cdef0c286db5fa22282c3629a56a52cebe
neruneruna7 <neruneruna7@gmail.com>
2026-07-01 15:18:21 +0900

    素数チェック

--- p2/src/bin/1.rs
+++ p2/src/bin/1.rs
@@ -1,24 +1,37 @@
 use num_bigint::{BigInt, RandBigInt as _, ToBigInt};
-use num_traits::{One as _, Zero};
-use rand::{Rng, rngs::ThreadRng};
+use num_traits::{Num as _, One as _, Zero};
+use rand::{
+    Rng, SeedableRng as _,
+    rngs::{SmallRng, ThreadRng},
+};
+
+fn main() {
+    // 1が500桁並んだ整数を作る
+    let one_five_hundred_str = "1".repeat(500);
+    let one_five_hundred = BigInt::from_str_radix(&one_five_hundred_str, 10).unwrap();
+    println!("1が500個並んでいるはず: {}", one_five_hundred);
+    // 素数が見つかるまで
+    // let mut rng = rand::thread_rng();
+    let mut rng = SmallRng::from_entropy();
+
+    let mut i = BigInt::zero();
+    let prime_num = loop {
+        let current_num = &one_five_hundred + &i;
+        if miller_rabin(current_num.clone(), 20, &mut rng) == true {
+            break current_num;
+        }
+        i += BigInt::one();
+    };
+    println!("素数: {}", prime_num);
+
+    let check = miller_rabin(prime_num.clone(), 1000, &mut rng);
+    println!("素数かどうか再度チェック: {}", check);
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum MRResult {
     MayBePrime,
     Composite,
-}
-
-fn main() {
-    let mr = miller_rabin(3500000000000011_u128.to_bigint().unwrap());
-    println!("{}", mr);
-    let mr = miller_rabin(3500000000000033_u128.to_bigint().unwrap());
-    println!("{}", mr);
-    let mr = miller_rabin(3500000000000059_u128.to_bigint().unwrap());
-    println!("{}", mr);
-    let mr = miller_rabin(
-        3500000000000011_u128.to_bigint().unwrap() * 3500000000000059_u128.to_bigint().unwrap(),
-    );
-    println!("{}", mr);
 }
 
 fn my_miller_rabin1(p: BigInt, rng: &mut impl Rng) -> MRResult {
@@ -43,6 +56,10 @@ fn my_miller_rabin1(p: BigInt, rng: &mut impl Rng) -> MRResult {
         .map(|i| 2_usize.pow(i) * &d)
         // 0 <= forall i < s , a^{2^i d} != -1 (mod p)　を計算
         .all(|exp| a.modpow(&exp, &p) != p_minus_1);
+    // let x0 = a.modpow(&d, &p);
+    // let y = std::iter::successors(Some(x0), |x| Some((x * x) % &p))
+    //     .take(s as usize)
+    //     .any(|x| x == p_minus_1);
 
     if result_1 && result_2 {
         MRResult::Composite
@@ -64,11 +81,9 @@ fn decompose(p: &BigInt) -> (BigInt, u32) {
     (d, s)
 }
 
-fn miller_rabin(p: BigInt) -> bool {
-    let mut rng = rand::thread_rng();
-
-    for _ in 0..20 {
-        let my_result = my_miller_rabin1(p.clone(), &mut rng);
+fn miller_rabin(p: BigInt, itertions: usize, rng: &mut impl Rng) -> bool {
+    for _ in 0..itertions {
+        let my_result = my_miller_rabin1(p.clone(), rng);
         if my_result == MRResult::Composite {
             return false;
         }
