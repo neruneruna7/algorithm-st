6a87b4920bb0ab8f5bad6155aba46a76af772cdb
neruneruna7 <neruneruna7@gmail.com>
2026-07-01 19:05:31 +0900

    ローメソッドの改良2

--- p2/src/bin/2.rs
+++ p2/src/bin/2.rs
@@ -1,5 +1,5 @@
 use num_bigint::BigInt;
-use num_traits::{One, abs};
+use num_traits::{One, Zero, abs};
 
 fn gcd(x: &BigInt, y: &BigInt) -> BigInt {
     match (x, y) {
@@ -19,30 +19,41 @@ fn gg(x: &BigInt, n: &BigInt) -> BigInt {
 
 fn work() {
     let n = BigInt::from(9145356185469980673640298696124239_u128);
-    let mut x = BigInt::from(1);
-    let mut y = BigInt::from(1);
+    let mut width = BigInt::from(1);
+    let mut start = BigInt::from(0);
     let mut count = 0;
-    let mut count2 = 0;
-    let mut pp = BigInt::from(1);
+    let mut x0 = g(&BigInt::from(3), &n);
+    let mut p = BigInt::from(1);
     loop {
-        x = g(&x, &n);
-        y = gg(&y, &n);
-        let diff = &x - &y;
-        pp = (&abs(diff.clone()) * &pp) % &n;
-        if count2 % 100 == 0 {
-            let px = gcd(&pp, &n);
-            pp = BigInt::one();
-            if px > BigInt::from(1) {
-                break;
+        width *= &BigInt::from(2);
+        println!("width = {}", &width);
+        let mut x = x0.clone();
+
+        let end = &width - &BigInt::one();
+        // let i = (BigInt::zero()..(&width - &BigInt::one()));
+        let ite = std::iter::successors(Some(BigInt::zero()), |x| Some(x + BigInt::one()))
+            .take_while(move |x| x < &end);
+
+        for i in ite {
+            x = g(&x, &n);
+            let diff = &x - &x0;
+            p = (&p * &abs(diff)) % &n;
+            count += 1;
+            if count % 100 == 0 {
+                let k = gcd(&p, &n);
+                if k != BigInt::one() {
+                    println!("{k}");
+                    return;
+                }
+                p = BigInt::one();
+            }
+            if count % 1000000 == 0 {
+                println!("{count}");
             }
         }
-        count += 1;
-        count2 += 1;
-        if count % 100000 == 0 {
-            println!("Iteration: {count}");
-        }
+        x0 = g(&x, &n)
     }
-    println!("ans = {count}");
+    // println!("ans = {count}");
 }
 
 fn sigma_method() {
