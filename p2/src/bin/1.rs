467542282dc98746c5acf1b114c4f62ec44a2219
neruneruna7 <neruneruna7@gmail.com>
2026-07-01 13:50:06 +0900

    update:

--- p2/src/bin/1.rs
+++ p2/src/bin/1.rs
@@ -1,66 +1,72 @@
-use num_bigint::{BigUint, RandBigInt as _, ToBigUint};
+use num_bigint::{BigInt, RandBigInt as _, ToBigInt};
 use num_traits::{One as _, Zero};
-use rand::rngs::ThreadRng;
+use rand::{Rng, rngs::ThreadRng};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum MRResult {
+    MayBePrime,
+    Composite,
+}
 
 fn main() {
-    let mr = miller_rabin(3500000000000011_u128.to_biguint().unwrap());
+    let mr = miller_rabin(3500000000000011_u128.to_bigint().unwrap());
     println!("{}", mr);
-    let mr = miller_rabin(3500000000000033_u128.to_biguint().unwrap());
+    let mr = miller_rabin(3500000000000033_u128.to_bigint().unwrap());
     println!("{}", mr);
-    let mr = miller_rabin(3500000000000059_u128.to_biguint().unwrap());
+    let mr = miller_rabin(3500000000000059_u128.to_bigint().unwrap());
     println!("{}", mr);
     let mr = miller_rabin(
-        3500000000000011_u128.to_biguint().unwrap() * 3500000000000059_u128.to_biguint().unwrap(),
+        3500000000000011_u128.to_bigint().unwrap() * 3500000000000059_u128.to_bigint().unwrap(),
     );
     println!("{}", mr);
 }
 
-fn expr(a: &BigUint, n: &BigUint, p: &BigUint) -> BigUint {
-    if *n == BigUint::ZERO {
-        return BigUint::one();
+fn my_miller_rabin1(p: BigInt, rng: &mut impl Rng) -> MRResult {
+    // pを，p-1 = 2^s * d　に分解
+    let p_minus_1 = &p - BigInt::one();
+    let (d, s) = decompose(&p_minus_1);
+    // [1, p-1]の範囲からランダムにaを選ぶ
+    let a = rng.gen_bigint_range(&BigInt::from(1_usize), &p_minus_1);
+    // a^d != 1 (mod p)
+    // かつ
+    // 0 <= forall i < s , a^{2^i d} != -1 (mod p)
+    // であるならば，合成数
+    // 出なければ，多分素数
+
+    // a^d != 1 (mod p)
+    let result_1 = a.modpow(&d, &p) == BigInt::one();
+    let result_2 = (0..s)
+        // 指数の計算
+        .map(|i| 2_usize.pow(i) * &d)
+        // 0 <= forall i < s , a^{2^i d} != -1 (mod p)　を計算
+        .all(|exp| a.modpow(&exp, &p) != BigInt::from(-1));
+
+    if result_1 && result_2 {
+        MRResult::Composite
     } else {
-        let next_n = n / 2.to_biguint().unwrap();
-        let m = expr(a, &next_n, p);
-        if n % 2.to_biguint().unwrap() == BigUint::zero() {
-            return m.pow(2) % p;
-        } else {
-            return m.pow(2) * a % p;
-        }
+        MRResult::MayBePrime
     }
 }
 
-fn miller_rabin1(p: BigUint, rng: &mut ThreadRng) -> bool {
-    let low = 0.to_biguint().unwrap();
-    let high = 10.to_biguint().unwrap();
-    let mut n = &p - BigUint::one();
-    while &n % 2.to_biguint().unwrap() == BigUint::zero() {
-        n /= 2.to_biguint().unwrap();
+fn decompose(p: &BigInt) -> (BigInt, u32) {
+    let two = BigInt::from(2_u32);
+
+    let mut s = 0;
+    let mut d = p.clone();
+    while (&d % &two).is_zero() {
+        d = &d / &two;
+        s += 1;
     }
-    let mut a = BigUint::one();
-    for _ in 0..100 {
-        let random = rng.gen_biguint_range(&low, &high);
-        a = 10.to_biguint().unwrap() * a + random;
-    }
-    a = a % &p;
-    let mut m = expr(&a, &n, &p);
-    if &m == &BigUint::one() || &m == &(&p - BigUint::one()) {
-        return true;
-    }
-    while n <= (p.clone() - BigUint::one()) {
-        m = m.pow(2) % p.clone();
-        if (&m + &BigUint::one()) % &p == BigUint::zero() {
-            return true;
-        }
-        n *= 2.to_biguint().unwrap();
-    }
-    return false;
+
+    (d, s)
 }
 
-fn miller_rabin(p: BigUint) -> bool {
+fn miller_rabin(p: BigInt) -> bool {
     let mut rng = rand::thread_rng();
 
     for _ in 0..20 {
-        if !miller_rabin1(p.clone(), &mut rng) {
+        let my_result = my_miller_rabin1(p.clone(), &mut rng);
+        if my_result == MRResult::Composite {
             return false;
         }
     }
