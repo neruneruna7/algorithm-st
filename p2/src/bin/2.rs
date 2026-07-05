c7d7b40168a51a77b28e1bb2cc6c1dbdbc42ef71
neruneruna7 <neruneruna7@gmail.com>
2026-07-05 15:59:24 +0900

    lint

--- p2/src/bin/2.rs
+++ p2/src/bin/2.rs
@@ -1,19 +1,12 @@
 use num_bigint::BigInt;
-use num_traits::{One, Zero, abs};
 
-use num_bigint::RandBigInt as _;
-use p2::{
-    miller_rabin::miller_rabin,
-    qs::{self, quadratic_sieve1},
-    quadratic_sieve::quadratic_sieve,
-    rho::rho_method,
-};
-use rand::{Rng, SeedableRng as _, rngs::SmallRng};
+use p2::qs::quadratic_sieve1;
+use rand::{SeedableRng as _, rngs::SmallRng};
 
 fn main() {
-    let q = BigInt::from(914535618546997293219643669199126899_u128);
-    let q = BigInt::from(128);
-    let q = BigInt::from(405003390007_u128);
+    let _q = BigInt::from(914535618546997293219643669199126899_u128);
+    let _q = BigInt::from(128);
+    let _q = BigInt::from(405003390007_u128);
     let q = BigInt::from(11629360743077306442685712558623_u128);
     // println!("{q}");
     // // todo!("課題2")
