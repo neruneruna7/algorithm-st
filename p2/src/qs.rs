73ba7cca78d4d3180d787342b6a535a250fc2eae
neruneruna7 <neruneruna7@gmail.com>
2026-07-05 15:26:47 +0900

    Update qs.rs

--- p2/src/qs.rs
+++ p2/src/qs.rs
@@ -1,8 +1,15 @@
 use num_bigint::BigInt;
-use num_traits::{One as _, Signed as _, Zero as _};
+use num_traits::{One as _, Signed as _, ToPrimitive as _, Zero as _};
 use rand::Rng;
 
 use crate::{miller_rabin::miller_rabin, rho::rho_method};
+
+/// 200以下の素数一覧
+const FACTOR_BASE: [u64; 46] = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+    197, 199,
+];
 
 fn prime_factorize(n: &BigInt, rng: &mut impl Rng) -> Vec<BigInt> {
     let one = BigInt::one();
@@ -49,12 +56,43 @@ fn prime_factorize(n: &BigInt, rng: &mut impl Rng) -> Vec<BigInt> {
     factors
 }
 
+/// 素因数を指数としたベクトルと、各素因数の偶奇性を表すベクトルに変換する
+fn factors_to_vectors(factors: &[BigInt]) -> Option<(Vec<u32>, Vec<u8>)> {
+    let minus_one = BigInt::from(-1);
+    let mut exponents = vec![0_u32; FACTOR_BASE.len() + 1];
+
+    for f in factors {
+        if f == &minus_one {
+            exponents[0] += 1;
+            continue;
+        }
+
+        let p = f.to_u64()?;
+
+        let pos = FACTOR_BASE.iter().position(|&q| q == p)?;
+
+        exponents[pos + 1] += 1;
+    }
+
+    let parity = exponents.iter().map(|e| (e & 1) as u8).collect();
+
+    Some((exponents, parity))
+}
+
+#[derive(Debug, Clone)]
+struct Relation {
+    x: i32,
+    qx: BigInt,
+    exponents: Vec<u32>,
+    parity: Vec<u8>,
+}
+
 pub fn quadratic_sieve1(n: &BigInt, rng: &mut impl Rng) -> BigInt {
     // 2次ふるい法
     //　因数分解したい数をn
     let m = n.sqrt();
     let x = -8000..8000;
-    let factors_vec = x
+    let factors_iter = x
         .filter_map(|x| {
             let x_tilde = x + &m;
             let qx: BigInt = &x_tilde * &x_tilde - n;
@@ -79,12 +117,26 @@ pub fn quadratic_sieve1(n: &BigInt, rng: &mut impl Rng) -> BigInt {
             Some((x, if is_negative { -qx_abs } else { qx_abs }, factors))
         })
         // 200以下の素因子を持つものに絞る
-        .filter(|v| v.2.iter().all(|f| *f <= BigInt::from(200)))
+        .filter(|v| v.2.iter().all(|f| *f <= BigInt::from(200)));
+    let factors_vec = factors_iter
+        .filter_map(|(x, qz, f)| {
+            let (exponents, parity) = factors_to_vectors(&f)?;
+            Some(Relation {
+                x,
+                qx: qz,
+                exponents,
+                parity,
+            })
+        })
         .collect::<Vec<_>>();
 
-    factors_vec.iter().for_each(|(x, qx, factors)| {
-        println!("factors: x={} qx={:?} factors={:?}", x, qx, factors);
+    factors_vec.iter().for_each(|i| {
+        println!("{:?}", i);
     });
+
+    // factors_iter.for_each(|(x, qx, factors)| {
+    //     println!("factors: x={} qx={:?} factors={:?}", x, qx, factors);
+    // });
 
     todo!()
 }
