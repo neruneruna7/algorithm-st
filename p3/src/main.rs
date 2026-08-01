db23eba3d8daac9628a0c52bf6158134aa44f3a8
neruneruna7 <neruneruna7@gmail.com>
2026-08-01 13:28:22 +0900

    ４つの要素をソートするソーティングネットワーク

--- p3/src/main.rs
+++ p3/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
+    let input = (true, false, true, false);
+    let output = sorting_network_4(input);
+    println!("{:?}", output);
+
+    // let input_bitonic = None;
+}
+
+/// 左を上，右を下とみなす．
+fn comparator(x: bool, y: bool) -> (bool, bool) {
+    if x > y { (x, y) } else { (y, x) }
+}
+
+fn sorting_network_4(input: (bool, bool, bool, bool)) -> (bool, bool, bool, bool) {
+    let (step1_1, step1_2) = comparator(input.0, input.1);
+    let (step1_3, step1_4) = comparator(input.2, input.3);
+
+    let (step2_1, step2_2) = comparator(step1_1, step1_3);
+    let (step2_3, step2_4) = comparator(step1_2, step1_4);
+
+    let (step3_2, step3_3) = comparator(step2_2, step2_3);
+    let (step3_1, step3_4) = (step2_1, step2_4);
+
+    (step3_1, step3_2, step3_3, step3_4)
 }
